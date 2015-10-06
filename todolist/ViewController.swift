bbd0da625f6c71dd485b255a0352916d868369b2
Alex Zhang <alex.zhang@berkeley.edu>
2015-10-05 21:03:13 -0700

    Initial Commit

---
+++ todolist/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  todolist
+//
+//  Created by Alex Zhang on 10/5/15.
+//  Copyright © 2015 Alex Zhang. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
